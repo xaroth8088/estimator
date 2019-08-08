2dc6433f60e3f8a6fe8bc6cdb80561881c3268e1
Josh <jrogan@wikia-inc.com>
2019-08-07 17:51:55 -0700

    Update host

--- build/bundle.js
+++ build/bundle.js
@@ -52844,7 +52844,7 @@
 	    value: true
 	});
 	var CONFIG = {
-	    crossbar_uri: 'ws://157.230.143.136:8080/ws',
+	    crossbar_uri: 'ws://35.233.145.83:8080/ws', // update this to the k8s url
 	    realm: 'realm1'
 	};
 
--- src/config.es6
+++ src/config.es6
@@ -1,5 +1,5 @@
 const CONFIG = {
-    crossbar_uri: 'ws://157.230.143.136:8080/ws', // update this to the k8s url
+    crossbar_uri: 'ws://35.233.145.83:8080/ws', // update this to the k8s url
     realm: 'realm1'
 };
 
